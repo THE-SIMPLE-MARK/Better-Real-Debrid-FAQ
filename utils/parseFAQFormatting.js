70a7b014bcb9447c33ee32f3fca705cf6e8ddd01
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2023-07-24 22:21:09 +0200

    Fix formatting parsing being case-sensitive

--- utils/parseFAQFormatting.js
+++ utils/parseFAQFormatting.js
@@ -1,22 +1,22 @@
 export default function parseFAQFormatting(text) {
 	// bold
-	text = text.replace(/\[b\](.*?)\[\/b\]/g, "<strong>$1</strong>")
+	text = text.replace(/\[b\](.*?)\[\/b\]/gi, "<strong>$1</strong>")
 
 	// italic
-	text = text.replace(/\[i\](.*?)\[\/i\]/g, "<em>$1</em>")
+	text = text.replace(/\[i\](.*?)\[\/i\]/gi, "<em>$1</em>")
 
 	// underline
-	text = text.replace(/\[u\](.*?)\[\/u\]/g, "<u>$1</u>")
+	text = text.replace(/\[u\](.*?)\[\/u\]/gi, "<u>$1</u>")
 
 	// hyperlinks without accompanying text
 	text = text.replace(
-		/\[url\](.*?)\[\/url\]/g,
+		/\[url\](.*?)\[\/url\]/gi,
 		'<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>'
 	)
 
 	// hyperlinks with accompanying text
 	text = text.replace(
-		/\[url=(.*?)\](.*?)\[\/url\]/g,
+		/\[url=(.*?)\](.*?)\[\/url\]/gi,
 		'<a href="$1" target="_blank" rel="noopener noreferrer">$2</a>'
 	)
 
@@ -28,28 +28,28 @@ export default function parseFAQFormatting(text) {
 
 	// quotes
 	text = text.replace(
-		/\[quote\](.*?)\[\/quote\]/g,
+		/\[quote\](.*?)\[\/quote\]/gi,
 		"<blockquote>$1</blockquote>"
 	)
 
 	// lists
-	text = text.replace(/\[list\](.*?)\[\/list\]/g, "<ul>$1</ul>")
-	text = text.replace(/\[list=1\](.*?)\[\/list\]/g, "<ol>$1</ol>")
-	text = text.replace(/\[\*\](.*?)(?=\[\/list\])/g, "<li>$1</li>")
+	text = text.replace(/\[list\](.*?)\[\/list\]/gi, "<ul>$1</ul>")
+	text = text.replace(/\[list=1\](.*?)\[\/list\]/gi, "<ol>$1</ol>")
+	text = text.replace(/\[\*\](.*?)(?=\[\/list\])/gi, "<li>$1</li>")
 
 	// headers
-	text = text.replace(/\[h1\](.*?)\[\/h1\]/g, "<h1>$1</h1>")
-	text = text.replace(/\[h2\](.*?)\[\/h2\]/g, "<h2>$1</h2>")
-	text = text.replace(/\[h3\](.*?)\[\/h3\]/g, "<h3>$1</h3>")
+	text = text.replace(/\[h1\](.*?)\[\/h1\]/gi, "<h1>$1</h1>")
+	text = text.replace(/\[h2\](.*?)\[\/h2\]/gi, "<h2>$1</h2>")
+	text = text.replace(/\[h3\](.*?)\[\/h3\]/gi, "<h3>$1</h3>")
 
 	// text color
 	text = text.replace(
-		/\[color=(#[0-9A-Fa-f]{6})\](.*?)\[\/color\]/g,
+		/\[color=(#[0-9A-Fa-f]{6})\](.*?)\[\/color\]/gi,
 		'<span style="color: $1;">$2</span>'
 	)
 
 	// new lines
-	text = text.replace(/\n/g, "<br>")
+	text = text.replace(/\n/gi, "<br>")
 
 	return <div dangerouslySetInnerHTML={{ __html: text }} />
 }
